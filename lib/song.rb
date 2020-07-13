ce168933aad4961b83348c5a454873591797796c
Andrea Tchalaou <attchalaou@gmail.com>
2020-07-13 08:44:03 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -53,3 +53,9 @@ end
 end
 
 ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+mask_off = Song.new("Mask Off", "Future", "Rap")
+dance_macabre = Song.new("Dance Macabre", "Ghost", "Metal")
+if_you_have_ghosts = Song.new("If You Have Ghosts", "Ghost", "Metal")
+Song.genres
+p Song.artist_count
+p Song.genre_count

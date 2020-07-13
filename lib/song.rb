1aae5af252d0956130bc9eddc2626b8ee5daf9b2
Andrea Tchalaou <attchalaou@gmail.com>
2020-07-13 08:24:44 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,41 @@
+class Song 
+  
+  @@song_count = 0
+   @@genre = {}
+  @@artists = {}
+  
+  def initialize
+    @@song_count +=1
+    @@genre 
+    @@artists
+  end
+  
+ def self.count
+   @@song_count
+ end
+ 
+ def genre
+ 
+ def self.artists
+ 
+  attr_accessor :name, :artists, :genre
+  
+  def initialize()
+    @
+  end
+  
+  def()
+  @
+end
+end
+
+
+ninety_nine_problems = Song.new("99 Problems", "Jay-Z", "rap")
+ninety_nine_problems.name
+ninety_nine_problems.artists
+ninety_nine_problems.genre
+Song.count
+Song.artists
+Song.genre
+Song.genre_count
+Song.artist_count
